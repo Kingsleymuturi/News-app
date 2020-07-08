76ae80c75bbeb3f386af0cb615908393dc039918
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-07-08 09:27:47 +0300

    add start.sh file

--- manage.py
+++ manage.py
@@ -1,5 +1,19 @@
 from app import create_app
+from flask_script import Manager, Server
+
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """
+    Running all the unit tests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == "__main__":
-    app.run()
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,3 @@
+export API_KEY='992aa3eee0b140a691feac60ece1e2ba'
+
+python3 manage.py server
